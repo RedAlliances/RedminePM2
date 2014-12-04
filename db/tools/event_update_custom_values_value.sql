3a3401c47e1933d002e5564da66270521aa1ddc0
Roy  T.Burns <roy.burns@163.com>
2014-12-04 15:23:47 +0800

    subject列太长，截断一下。

--- db/tools/event_update_custom_values_value.sql
+++ db/tools/event_update_custom_values_value.sql
@@ -1,5 +1,9 @@
 # 用来设置自定义属性“根任务作者”，实际上就是在这个表里创建一个关联
 # “根任务作者”，任务链里面顶层任务的创建者，用来过滤类似“任务单”这个的任务链。
+# customized_id ==> issues.id
+# custom_field_id ==> custom_fields.id
+# value ==> issues.root_id.author_id
+
 update custom_values t
 set   t.value = (
 			SELECT ta.author_id
